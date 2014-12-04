a4c92f571f70a6300a62932ca3bc9e56cf684b38
Ignacio Rodríguez <ignacio@sugarlabs.org>
2014-12-03 23:34:32 -0200

    Convert PO files to INI.
    ConvertPOTtoINI.py -> run the script with .pot file.
    script.py mypot.pot
    
    ConvertPOtoINI.py -> run the script with .po file.
    script.py mypo.po
    
    ConvertPOStoINI.py -> run the script, and put all .pot - .po files.
    Will be converted automatic and saved in myini.ini
    cd po/
    script.py

---
+++ ConvertPOTtoINI.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2014 - Ignacio Rodríguez <ignacio@sugarlabs.org>
+# This is for the .pot file.
+# How to puse: script.py file.pot, it will write a file.ini
+
+import os
+import sys
+
+
+def convert_pot_to_ini(filename):
+    filename = sys.argv[1]
+    text = open(filename, "r").read()
+    lang = os.path.basename(filename)[:-4]  # -4 = .pot
+    finaltext = "[" + lang + "]\n"
+
+    REPLACE = [
+        ",",
+        "(",
+        ")",
+        "?",
+        "¿",
+        "<",
+        ">",
+        ".",
+        '"\n',
+        '"',
+        ":",
+        "%s",
+        "%d",
+        "/",
+        "'",
+        ";",
+        "×"]
+
+    msgid = text.find("msgid")
+    msgstr = text.find("msgstr")
+    while msgid >= 0:
+        got = text[msgid:msgstr - 1][7:-1]
+        for x in REPLACE:
+            got = got.replace(x, "")
+
+        got = got.replace(" ", "-")
+        txt = got + ' = \n'
+        if not txt.startswith(" = "):
+            finaltext += txt
+        text = text[msgstr:]
+        enter = text.find("\n")
+        text = text[enter:]
+        msgid = text.find("msgid")
+        msgstr = text.find("msgstr")
+
+    return finaltext, lang
+
+if __name__ == '__main__':
+    ini = convert_pot_to_ini(sys.argv[1])
+    text_n = open(ini[1] + ".ini", "w")
+    text_n.write(ini[0])
+    text_n.close()
+    print "Finished"
---
+++ ConvertPOstoINI.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2014 - Ignacio Rodríguez <ignacio@sugarlabs.org>
+# Convert all *.po files to ini, and put all in one
+# How to use: python script.py
+
+import os
+
+from ConvertPOtoINI import convert_po_to_ini
+
+print "Saving to myini.ini"
+
+try:
+    os.remove("myini.ini")
+except:
+    pass
+
+dirlist = os.listdir(".")
+final_ini = ""
+
+total = 0
+for fil_e in dirlist:
+    if fil_e[-3:] == ".po" or fil_e[-4:] == ".pot":
+        try:
+            txt = convert_po_to_ini(fil_e)
+            total += 1
+        except:
+            print "Error with %s." % fil_e
+            continue
+
+        final_ini += txt[0] + "\n"
+        print fil_e
+
+if not total:
+    print "No PO or POT files to convert."
+    exit()
+text_n = open("myini.ini", "w")
+text_n.write(final_ini)
+text_n.close()
